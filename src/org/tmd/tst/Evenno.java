42f0b211976b9b9109ac85613fb64956c2494f4f
Karthik271991 <Karthikkeyan199127@gmail.com>
2019-09-12 12:20:10 +0530

    this is my project

---
+++ src/org/tmd/tst/Evenno.java
@@ -0,0 +1,15 @@
+package org.tmd.tst;
+
+public class Evenno {
+	public static void main(String[] args) {
+		int c1=0;
+		for(int i=0;i<=50;i++)
+		if(i%2==0)
+		{
+			c1=c1+i;
+		}
+		System.out.println("Sum of even no :"+c1);
+	}
+
+
+}
